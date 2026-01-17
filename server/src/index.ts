484ace5abd1d71b60440294cef7c883dc2ed118b
aniket kumar jha <146815167+aniket4518@users.noreply.github.com>
2026-01-17 21:11:39 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,21 +4,11 @@ import Groq from "groq-sdk";
 import { generatePrompt } from "./prompt/prompt";
 import { parseCodeResponse } from "./utils/fileParser";
 import cors from "cors";
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://webmaker-nine.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+ 
 const app = express();
+ app.use(cors());
 app.use(express.json());
+ 
 app.use(express.urlencoded({ extended: true }));  
  
 const GROQ_API_KEY = process.env.GROQ_API_KEY;
